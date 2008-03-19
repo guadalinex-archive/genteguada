a1b0f02250186b6df8486dcb4b3374a431e70f86
mdelacalle <mdelacalle@b6332b80-7740-0410-afe7-f4c4f72231bb>
2008-03-19 17:38:39 +0000

    Eliminamos la version anterior

--- subject_hud.py
+++
@@ -1,9 +0,0 @@
-from subject import *
-
-#******************************************************************************
-# CLASE SUB_HUD (subclase de SUBJECT)
-# Subject de tipo HUD
-
-class SubjectHud(Subject):
-
-  pass
