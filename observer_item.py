11ee05a3d17ff4c1a254af87c8a4dcd7b24df571
mdelacalle <mdelacalle>
2008-03-19 17:38:39 +0000

    Eliminamos la version anterior

--- observer_item.py
+++
@@ -1,9 +0,0 @@
-from observer import *
-
-#******************************************************************************
-# CLASE OBS_ITEM (subclase de OBSERVER)
-# Observador de un item generico
-
-class ObserverItem(Observer):
-
-  pass
