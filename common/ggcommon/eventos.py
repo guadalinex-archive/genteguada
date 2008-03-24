eff7d638d45d07b1ca7d49f903f18031e0a4f5d8
mdelacalle <mdelacalle@b6332b80-7740-0410-afe7-f4c4f72231bb>
2008-03-24 11:41:06 +0000

    comenzando con el paquete common

---
+++ common/ggcommon/eventos.py
@@ -0,0 +1,14 @@
+
+class Event:
+
+  """
+  Clase padre de los eventos, todos los eventos definidos en el juego
+  tienen que heredar de esta clase
+  """
+
+  def __init__(self):
+    """
+    Constructor de la clase
+    """
+    self.name = "Evento Padre"
+
