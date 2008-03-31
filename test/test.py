75e479f6a9a11c2e315a3fc771b717b2589e4727
mdelacalle <mdelacalle@b6332b80-7740-0410-afe7-f4c4f72231bb>
2008-03-31 14:19:42 +0000

    Despues de la prueba vuelve al estado correcto

--- test/test.py
+++ test/test.py
@@ -30,7 +30,7 @@ class TestEventos(unittest.TestCase):
     assert self.eventCount == 1
     assert self.lastEvent.params['destination'] == [2, 0, 1]
     assert self.lastEvent.producer == player
-    assert self.lastEvent.name == 'destinationte'
+    assert self.lastEvent.name == 'destination'
 
   def testDeleteEventByType(self):
     player = ggclient.player.Player("player", 0, PLAYER_SPRITE1, CHAR_SZ, (2, 0, 2))
