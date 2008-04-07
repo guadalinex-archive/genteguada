9dca45dc7c2d58b6b74bcf494d29184d0b077278
mdelacalle <mdelacalle@b6332b80-7740-0410-afe7-f4c4f72231bb>
2008-04-07 15:44:33 +0000

    Nuestro parte remota en el cliente

---
+++ client/ggclient/remoteclient.py
@@ -0,0 +1,84 @@
+import threading
+import socket
+import pickle
+import sys
+
+class RClientThread(threading.Thread): #{{{
+
+  def __init__(self, rClient):
+    threading.Thread.__init__(self)
+    self.rClient = rClient
+    self.socket  = None
+
+  def _receiveRootModel(self):
+    data = self.socket.recv(1024)
+    rootModel = pickle.loads(data)
+    self.rClient.setRootModel(rootModel)
+
+  def run(self):
+    self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.socket.connect((self.rClient._serverIP, self.rClient._serverPort))
+    self._receiveRootModel()
+    while True:
+      data = raw_input('')
+      if data.rstrip() == "quit": 
+        self.socket.close()
+        sys.exit(0)
+
+  def sendCommand(self, command):
+    serializedCommand = pickle.dumps(command)
+    self.socket.send(serializedCommand)
+    answerData = self.socket.recv(1024)
+    answer = pickle.loads(answerData)
+    return answer
+#}}}
+
+#{{{ class RClient
+
+global _rClientSingleton
+_rClientSingleton = None
+
+def getRClient():
+  if _rClientSingleton == None:
+    raise Exception("RClient has to be instanciated before calling getRClient()")
+  return _rClientSingleton
+
+
+class RClient: 
+
+  def __init__(self, serverIP, port=8000):
+    global _rClientSingleton
+    if not (_rClientSingleton == None):
+      raise Exception("Can't create more then one instance of RClient")
+    _rClientSingleton = self
+    self._serverIP = serverIP
+    self._serverPort = port
+    self._rootModel = None
+    self._rootModelSemaphore = threading.Semaphore(0)
+    self._thread = None
+    self._start()
+
+  def hasRootModel(self):
+    return not (self._rootModel == None)
+
+  def setRootModel(self, model):
+    if self._rootModel != None:
+      raise Exception('The receiver already has a rootModel')
+    self._rootModel = model
+    self._rootModelSemaphore.release()
+
+  def getRootModel(self):
+    self._rootModelSemaphore.acquire()
+    result = self._rootModel
+    self._rootModelSemaphore.release()   
+    return result
+
+  def sendCommand(self, command):
+    return self._thread.sendCommand(command)
+
+  def _start(self):
+    self._thread = RClientThread(self)
+    self._thread.start()
+
+#}}}
+
