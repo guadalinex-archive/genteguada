ac32bb20bf6c38f710420c496947e2d498dc34f9
mdelacalle <mdelacalle@b6332b80-7740-0410-afe7-f4c4f72231bb>
2008-04-14 11:47:07 +0000

    El tema de los eventos distribuidos funcionaba desde primera hora del viernes, el error estaba en los test

--- server/ggserver/remoteserver.py
+++ server/ggserver/remoteserver.py
@@ -10,6 +10,14 @@ import time
 import ggcommon.utils
 import struct
 import traceback
+import logging
+
+logger = logging.getLogger('genteguada')
+hdlr = logging.FileHandler('genteguada.log')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.DEBUG)
 
 
 # global to hold the rserver singleton instance
@@ -27,7 +35,9 @@ class RServer: #{{{
 
   def __init__(self, rootModel, port=8000): #{{{
     global _rServerSingleton
+    logger.debug("Iniciando servidor")
     if not (_rServerSingleton == None):
+      logger.error("Can't create more then one instance of RServer")
       raise Exception("Can't create more then one instance of RServer")
     _rServerSingleton = self
     self._models = {}
@@ -37,18 +47,22 @@ class RServer: #{{{
   #}}}
 
   def getModelByID(self, id): #{{{
+    logger.debug("Devolviendo el objeto modelo con id "+str(id))
     return self._models[id]
   #}}}
 
   def registerModel(self, model): #{{{
     if model in self._models.values():
+      logger.debug("Intentando registrar un modelo que ya esta registrado "+str(model))
       return
     modelID = ggcommon.utils.nextID()
     model.setID(modelID)
+    logger.debug("Registrando un modelo con id "+str(id))
     self._models[modelID] = model
   #}}}
 
   def getRootModel(self): #{{{
+    logger.debug("Devolviendo el rootModel")
     return self._rootModel
   #}}}
 
@@ -57,6 +71,7 @@ class RServer: #{{{
     con = SocketServer.ThreadingTCPServer(('', self._port), RServerHandler)
     con.request_queue_size = 500
     thread.start_new(con.serve_forever, ())
+    logger.info("Servidor puesto en escucha")
     print "servidor iniciado"
     while 1:
       data = raw_input('')
@@ -73,11 +88,13 @@ class RServerHandler(SocketServer.BaseRequestHandler): #{{{
     #_objectToSerialize = ggcommon.utils.objectToSerialize(getRServer().getRootModel(), getRServer())
     #serializedRootModel = pickle.dumps(_objectToSerialize)
     #self.request.send(serializedRootModel)
+    logger.debug("Enviando el root model")
     self._sendObject(getRServer().getRootModel())
   #}}}
 
   def setup(self): #{{{
     print self.client_address, 'conectado'
+    logger.info("Conectado el cliente "+str(self.client_address))
     self._sendRootModel()
   #}}}
 
@@ -88,67 +105,42 @@ class RServerHandler(SocketServer.BaseRequestHandler): #{{{
       if len(size):
         size = struct.unpack("i", size)[0]
         commandData = ""
+        logger.info("Recibimos del cliente algo con size "+str(size))
         while len(commandData) < size:
           commandData = self.request.recv(size - len(commandData))
         command = pickle.loads(commandData)
+        logger.info("Recibimos el comando "+str(command))
         command.setServerHandler(self)
         answer = command.do()
+        logger.info("Ejecutamos el comando "+str(command)+ " y obtenemos como respuesta "+str(answer))
         if answer:
+          logger.info("Enviamos la respuesta "+str(answer))
           self._sendObject(answer)
       else:
+        logger.info("Cerramos la conexion con "+str(self.client_address))
         break
-
-      """ 
-      commandData = self.request.recv(1024)
-      if (len(commandData) == 0):
-        time.sleep(0.01)
-      else:
-        print 'server received ' + str(len(commandData)) + 'bytes'
-        command = pickle.loads(commandData)
-        command.setServerHandler(self)
-        answer = command.do()
-        if answer:
-          self._sendObject(answer)
-        #answerToSerialize = ggcommon.utils.objectToSerialize(answer, getRServer())
-        #serializedAnswer = pickle.dumps(answerToSerialize)
-        #self.request.send(serializedAnswer)
-      """
   #}}}
 
-  """
-  def handle(self): #{{{
-    while 1:
-      commandData = self.request.recv(1024)
-      if (len(commandData) == 0):
-        time.sleep(0.01)
-      else:
-        print 'server received ' + str(len(commandData)) + 'bytes'
-        command = pickle.loads(commandData)
-        command.setServerHandler(self)
-        answer = command.do()
-        if answer:
-          self._sendObject(answer)
-        #answerToSerialize = ggcommon.utils.objectToSerialize(answer, getRServer())
-        #serializedAnswer = pickle.dumps(answerToSerialize)
-        #self.request.send(serializedAnswer)
-  #}}}
-  """
   def _sendObject(self, object):
     toSerialize = ggcommon.utils.objectToSerialize(object, getRServer())
+    logger.info("Serializamos el objeto "+str(object))
     serialized = pickle.dumps(toSerialize)
     try:
+      logger.info("Enviamos el objeto al cliente "+str(object))
       self.request.send(serialized)
       return True
     except:
+      logger.error("Ocurrio un error al enviar el "+str(object)+" al cliente "+str(self.client_address))
       print sys.exc_info()[1]
       traceback.print_exc()
       return False
-    #self.request.flush()
 
   def sendCommand(self, command):
+    logger.info("Enviamos comando")
     return self._sendObject(command)
 
   def finish(self): #{{{
+    logger.info("cerramos la conexion con el cliente "+str(self.client_address))
     print self.client_address, 'desconectado'
   #}}}
 
