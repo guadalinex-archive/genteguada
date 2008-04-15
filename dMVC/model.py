52fd0843a3de2519e1c8973e8948e3685c641346
Diego Gomez Deck <DiegoGomezDeck@users.noreply.github.com>
2008-04-15 11:29:21 +0000

    New module structure.

---
+++ dMVC/model.py
@@ -0,0 +1,77 @@
+import threading
+import ggcommon.remotemodel
+import traceback
+import copy
+
+class Model: #{{{
+
+  def __init__(self):
+    self._id = None
+    self._events = []
+    self._eventsMutex = threading.Semaphore(1)
+
+  #def hasID(self):
+  #  return (self._id != None)
+
+  def setID(self, id):
+    self._id = id
+
+  def __str__(self):
+    return '<Model ID: ' + str(self._id) +'>'
+
+  def objectToSerialize(self, server):
+
+    if self._id == None:
+      server.registerModel(self)
+
+    return ggcommon.remotemodel.RemoteModel(self._id)
+
+
+  def subscribeEvent(self, eventType, method):
+    """ Indica la accion a realizar ante un evento.
+    eventType: tipo de evento.
+    method: metodo que se lanzara.
+    """
+    self._eventsMutex.acquire()
+    self._events.append([eventType, method])
+    self._eventsMutex.release()
+
+    
+  def triggerEvent(self, eventType, **params):
+    """ Activa un evento.
+    eventType: tipo de evento.
+    params: datos sobre el evento.
+    """
+    self._eventsMutex.acquire()
+    eventsCopy = copy.copy(self._events)
+    self._eventsMutex.release()
+    for type, method in eventsCopy:
+      if type == eventType:
+        event = ggcommon.eventos.Event(self, eventType, params)
+        try:
+          method(event)
+        except:
+          traceback.print_exc()
+          print sys.exc_info()[1]
+
+    
+  def unsubscribeEventObserver(self, observer, eventType=None):
+    toRemove = []
+    self._eventsMutex.acquire()
+    for event in self._events:
+      if id(event[1].im_self) == id(observer):
+        if eventType == None or eventType == event[0]: 
+          toRemove.append(event)
+    for event in toRemove:
+      self._events.remove(event)
+    self._eventsMutex.release()
+
+  def unsubscribeEventMethod(self, method, eventType=None):
+    self._eventsMutex.acquire()
+    raise "actually do it!"
+    self._eventsMutex.release()
+
+
+  def __eq__(self, comparand):
+      return id(self) == id(comparand)
+#}}}
---
+++ dMVC/test/models.py
@@ -0,0 +1,89 @@
+import sys
+
+#sys.path.append("../igorpc")
+sys.path.append("../server")
+
+import ggserver.model
+
+class TestPlayer(ggserver.model.Model): #{{{
+    
+  def __init__(self):
+    ggserver.model.Model.__init__(self)
+
+  def name(self):
+    return 'maradona'
+#}}}
+
+class TestModel(ggserver.model.Model): #{{{
+    
+  def __init__(self):
+    ggserver.model.Model.__init__(self)
+    self._player = TestPlayer()
+    self._position = [0,0]
+    self._name = ''
+
+  def setPosition(self, position):
+      if self._position == position:
+          return
+
+      self._position = position
+      self.triggerEvent('position', position=self.getPosition())
+
+  def getPosition(self):
+      return self._position
+
+  def setName(self, name):
+      if self._name == name:
+          return
+
+      self._name = name
+      self.triggerEvent('name', name=self.getMyName())
+
+  def getMyName(self):
+      return self._name
+
+
+  def foo(self):
+    return 'foo'
+
+  def bar(self):
+    return 'bar'
+
+  def player(self):
+    return self._player
+
+  def sayHello(self,name):
+    return "Hello " + name
+
+  def metodoError(self):
+    result = 1/0
+    return result
+
+  def fullName(self, name, lastName):
+    return name + " " + lastName
+
+  def listPlayers(self):
+    return [TestPlayer(), TestPlayer()]
+
+  def tuplePlayers(self):
+    return (TestPlayer(), TestPlayer())
+
+  def dictPlayers(self):
+    return {"1":TestPlayer(), "2":TestPlayer()}
+
+  def getName(self,player):
+    return player.name()
+
+  def getListName(self,listplayer):
+    return listplayer[0].name()
+
+  def getTupleName(self,tupleplayer):
+    return tupleplayer[0].name()
+
+  def getDictName(self,dictplayer):
+    for key in dictplayer.keys():
+      name = dictplayer[key].name()
+    return name
+
+
+#}}}
