c8137a0742e1d93bd45d3d0146f95b72936ccc3a
emontana <emontana@b6332b80-7740-0410-afe7-f4c4f72231bb>
2008-05-07 15:36:26 +0000

    Inicio de modificaciones para incluir animacion en el movimiento de los jugadores.

--- gg/GG/isoview/isoview_item.py
+++ gg/GG/isoview/isoview_item.py
@@ -1,3 +1,4 @@
+import time
 import os
 import math
 import pygame
@@ -64,23 +65,32 @@ class IsoViewItem(isoview.IsoView):
   def animatedSetPosition(self):
     """ Creates a new animation and draws a player moving through the screen.
     """
-    self.__animation = animation.Animation(20, self.__position, self.getModel().getPosition())  
+    #print self.__img.rect.topleft, self.p3dToP2d(self.getModel().getPosition(), self.getModel().getOffset())
+    self.__animation = animation.Animation(GG.utils.MAX_FRAMES, self.__img.rect.topleft,
+                  self.p3dToP2d(self.getModel().getPosition(), self.getModel().getOffset()), self.__img)
 
   def frameUpdate(self):
-    
-    pass
-  
-  def draw(self, screen):
-    """ Runs some methods to paint on screen all players.
-    screen: screen handler.
-    """
-    self.__allPlayers.update()                     
-    self.__allPlayers.clear(screen, self.__bg.image)
-    pygame.display.update(self.__allPlayers.draw(screen))
-  
+    if self.__animation:
+      if not self.__animation.move():
+        del self.__animation
+        self.__animation = None
+        
+  def stopAnimation(self):
+    if self.__animation:
+      del self.__animation
+      self.__animation = None
+
   def positionChanged(self, event):
     """ Updates the item position and draws the room after receiving a position change event.
     event: even info.
     """
+    self.animatedSetPosition()
+    #print "================================ Nuevo movimiento: ", self.__img.rect.topleft
+    for i in range(0, GG.utils.MAX_FRAMES):
+      time.sleep(GG.utils.TICK_DELAY/GG.utils.MAX_FRAMES)
+      self.frameUpdate()
+      self.__ivroom.draw()
+    self.stopAnimation()  
+      
     self.__img.rect.topleft = self.p3dToP2d(event.getParams()["position"], self.getModel().getOffset())
     self.__ivroom.draw()
