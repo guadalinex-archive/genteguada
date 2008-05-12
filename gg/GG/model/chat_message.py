570b350e4f1c072a39ec8340b14bbeaa52ee2cff
mdelacalle <mdelacalle@b6332b80-7740-0410-afe7-f4c4f72231bb>
2008-05-12 16:47:06 +0000

    tenemos chat

--- gg/GG/model/chat_message.py
+++ gg/GG/model/chat_message.py
@@ -1,13 +1,17 @@
-
+import time
 
 class chatMessage:
     
   def __init__(self, message, sender):
     self.__message = message
     self.__sender = sender
+    self.__hour = time.time()
     
   def getMessage(self):
     return self.__message
 
   def getSender(self):
     return self.__sender
+
+  def getHour(self):
+    return time.strftime("%H:%M",time.localtime(self.__hour))
