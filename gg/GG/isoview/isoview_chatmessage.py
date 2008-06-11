22248de6992fd7d5329c87a00054dd723f77e424
mdelacalle <mdelacalle>
2008-06-11 16:21:26 +0000

    primera version del talk de los items

--- gg/GG/isoview/isoview_chatmessage.py
+++ gg/GG/isoview/isoview_chatmessage.py
@@ -21,16 +21,6 @@ class IsoViewChatMessage(positioned_view.PositionedView):
     pos = GG.utils.p3dToP2d(model.getPosition(), [0, 0])
     self.label.topleft = [pos[0] + 40, pos[1] - 30]
     self.__isohud.getIsoviewRoom().addTopSprite(self.label)
-    """
-    imgPath = GG.genteguada.GenteGuada.getInstance().getDataPath("balloon.png")  
-    self.__isohud = isohud
-    self.__img = pygame.sprite.Sprite()
-    self.__img.image = pygame.image.load(imgPath).convert_alpha()
-    self.__img.rect = self.__img.image.get_rect()
-    pos = GG.utils.p3dToP2d(model.getPosition(), [0, 0])
-    self.__img.rect.topleft = [pos[0] + 40, pos[1] - 30]
-    self.__isohud.getIsoviewRoom().addTopSprite(self.__img)
-    """
     
   def getImg(self):
     return self.label
@@ -41,22 +31,14 @@ class IsoViewChatMessage(positioned_view.PositionedView):
     #return self.__img.rect.topleft
 
   def setScreenPosition(self, pos):
-    #self.__img.rect.topleft = pos
     self.label.topleft = pos
   
-  #def setSprite(self, sprite):
-  #  self.__img.image = sprite
-    
   def getStyleMessageChat(self):
     """ Returns the chat current style.
     """
     #TODO entiendo que el color del chat depende de cada usuario 
     listStyle = ["chatEntryBlack","chatEntryRed","chatEntryGreen","chatEntryBlue"]
     return GG.utils.STYLES[listStyle[random.randint(0,len(listStyle)-1)]]
-  
-  def paintBalloon(self):
-    self.getScreen().blit(self.__img.image, GG.utils.HUD_OR)
-    pygame.display.update()
   
   def draw(self):
     hframe = ocempgui.widgets.HFrame()
@@ -74,11 +56,4 @@ class IsoViewChatMessage(positioned_view.PositionedView):
     label = GG.utils.OcempLabel(self.getModel().getMessage(),300)
     label.set_style(ocempgui.widgets.WidgetStyle(style))
     hframe.add_child(label)
-    """
-    string = self.getModel().getHour()+" [" + self.getModel().getSender() + "]: " + self.getModel().getMessage()
-    #label = ocempgui.widgets.Label(string)
-    label = GG.utils.OcempLabel(string,200)
-    label.set_style(ocempgui.widgets.WidgetStyle(self.getStyleMessageChat()))
-    hframe.add_child(label)
-    """
     return hframe
