052509fe58b96f9fddeafb9e3f1952bb43716725
emontana <emontana@b6332b80-7740-0410-afe7-f4c4f72231bb>
2008-06-30 15:57:15 +0000

    Introducida clase intermedia para items con inventario.

---
+++ gg/GG/model/item_with_inventory.py
@@ -0,0 +1,85 @@
+import GG.model.room_item
+import GG.utils
+import dMVC.model
+
+class GGItemWithInventory(GG.model.room_item.GGRoomItem):
+  """ GGItemWithInventory class.
+  """
+ 
+  def __init__(self, spritePath, position, anchor):
+    """ Class builder.
+    spriteList: sprite list used to paint the player.
+    position: player position.
+    anchor: image anchor on screen.
+    """
+    GG.model.room_item.GGRoomItem.__init__(self, spritePath, position, anchor)
+    self.__inventory = []
+    
+  # self.__inventory
+  
+  def setInventory(self, inventory):
+    """ Sets a new player's inventory.
+    inventory: new player's inventory.
+    """
+    if not self.__inventory == inventory:
+      self.__inventory = inventory
+      self.triggerEvent('inventory', inventory=inventory)
+      return True
+    return False
+  
+  def hasItemLabeledInInventory(self, label):
+    for item in self.__inventory:
+      if item.label == label:
+        return True  
+    return False    
+      
+  def addToInventoryFromRoom(self, item):
+    self.__inventory.append(item)
+    item.setPlayer(self)
+    self.triggerEvent('addToInventory', item=item, position=item.getPosition())
+    
+  def addToInventoryFromVoid(self, item, position):
+    self.__inventory.append(item)
+    item.setPlayer(self)
+    self.triggerEvent('addToInventory', item=item, position=position)
+    
+  def removeFromInventory(self, item):
+    """ Removes an item from the player's inventory.
+    item: item to be removed.
+    """
+    if item in self.__inventory:
+      self.__inventory.remove(item)
+      #item.setPlayer(None)
+      self.triggerEvent('removeFromInventory', item=item)
+      return True
+    return False
+  
+  def addToRoomFromInventory(self, item, dropLocation):
+    """ Removes an item from the inventory and drops it in front of the player.
+    item: item to drop.
+    dropLocation: item's drop location.
+    """
+    itemOnPosition = self.getRoom().getItemOnPosition(dropLocation)
+    if dropLocation == [-1, -1, -1]: 
+      return False
+    if itemOnPosition != None:
+      if not itemOnPosition.isStackable():
+        return False
+    if not self.getRoom().addItemFromInventory(item, dropLocation):
+      return False
+    self.__inventory.remove(item)
+    item.setPlayer(None)
+    self.triggerEvent('chat', actor=item, receiver=self, msg=item.label+" depositado en el suelo")
+    
+  def checkItemOnInventory(self, item):
+    for it in self.__inventory:
+      if it.checkSimilarity(item):
+        return True
+    return False      
+ 
+  def tick(self, now):
+    for item in self.__inventory:
+      item.tick(now)
+    
+  
+      
