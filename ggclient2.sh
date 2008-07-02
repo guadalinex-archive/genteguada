236000f4a0fa0ce0a4c5c9e7c5389515ca957844
emontana <emontana@b6332b80-7740-0410-afe7-f4c4f72231bb>
2008-07-02 08:30:00 +0000

    Arreglado fallo en busqueda de celdas ocupadas.

--- ggclient2.sh
+++ ggclient2.sh
@@ -6,4 +6,5 @@ echo "-----------------"
 echo
 
 export PYTHONPATH="./dmvc"
-python gg/genteguada -i 192.168.0.39 -u pepe2 -p 12345
+#python gg/genteguada -i 192.168.0.39 -u pepe2 -p 12345
+python gg/genteguada -i 127.0.0.1 -u pepe2 -p 12345
