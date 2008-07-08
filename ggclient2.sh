5d140cc2d1cf8c3d3c025236cb33e8b9653bd04c
mdelacalle <mdelacalle@b6332b80-7740-0410-afe7-f4c4f72231bb>
2008-07-08 08:21:51 +0000

    El hud es un box y ya tenemos tooltips en los botones

--- ggclient2.sh
+++ ggclient2.sh
@@ -7,4 +7,5 @@ echo
 
 export PYTHONPATH="./dmvc"
 #python gg/genteguada -i 192.168.0.39 -u pepe2 -p 12345
-python gg/genteguada -i 127.0.0.1 -u pepe2 -p 12345
+#python gg/genteguada -i 127.0.0.1 -u pepe2 -p 12345
+python gg/genteguada -i 192.168.0.102 -u pepe2 -p 12345
