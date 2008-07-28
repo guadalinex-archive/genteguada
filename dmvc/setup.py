919d42ddeaa46f2a97ddf04fb14dc4c8c0f5ac60
mdelacalle <mdelacalle>
2008-07-28 12:15:42 +0000

    soluciones en el empaquetamiento

--- dmvc/setup.py
+++ dmvc/setup.py
@@ -1,43 +1,9 @@
 #!/usr/bin/python
 
 from distutils.core import setup
-import os
-import sys
 import setuptools
 
-class doc(setuptools.Command):
-  description = "Genera la documentacion del proyecto"
-  user_options = []
-  doc_src=  "dMVC"
-  doc_dst="doc"
-  excepciones=['__init__.py']# Estos no los quiero documentar
-  
-  def initialize_options(self):
-    pass
-
-  def finalize_options(self):
-    pass
-
-  def run(self):
-    try:
-      import pydoc
-    except:
-      print "No se encuentra pydoc. No puedo seguir"
-      sys.exit(256)
-    
-    if not os.path.isdir(self.doc_dst):
-      os.mkdir(self.doc_dst)
-    os.chdir(self.doc_dst)
-    print "Generando documentacion de %s..."%self.doc_src,
-    pydoc.writedoc(self.doc_src)
-    for file in os.listdir('../'+self.doc_src):
-      if file.split('.')[-1] == 'py' and file not in self.excepciones:
-          print "Generando documentacion de %s..."%file,
-          pydoc.writedoc("%s.%s"%(self.doc_src,file.split('.')[0]))
-
-
 setup (  
-        cmdclass={"doc": doc},
         name = "dMVC",
         version = "0.1",
         description = "distributed model view controller",
