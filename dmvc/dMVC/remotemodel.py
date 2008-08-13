651c29840a150f76fa62e165ce70f05d89716a85
mdelacalle <mdelacalle>
2008-08-13 10:40:20 +0000

    ponemos de forma segura el tema de las animaciones

--- dmvc/dMVC/remotemodel.py
+++ dmvc/dMVC/remotemodel.py
@@ -6,14 +6,16 @@ import utils
 import new
 
 import thread
+import synchronized
 
-class RemoteModel: #{{{
+class RemoteModel(synchronized.Synchronized): #{{{
 
   def __init__(self, modelID, modelModuleName, modelClassName, variablesDict): #{{{
     self.__modelID         = modelID
     self.__modelModuleName = modelModuleName
     self.__modelClassName  = modelClassName
     self.__variablesDict   = variablesDict
+    synchronized.Synchronized.__init__(self)
   #}}}
 
   
