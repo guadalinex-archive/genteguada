ac12feb7706ef249b305a57b9a95bd60529d4f76
mdelacalle <mdelacalle@b6332b80-7740-0410-afe7-f4c4f72231bb>
2008-11-03 11:18:56 +0000

    mejorado el servidor http

--- gghttpserver/cgi/removegift.py
+++ gghttpserver/cgi/removegift.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
 
 import cgi
-import os
-import pickle
 import GG.model.ggsystem
 
 print "Content-Type: text/plain\r\n\r\n"
@@ -12,34 +10,9 @@ name = form.getvalue("user")
 idgift = form.getvalue("id")
 try:
   system = GG.model.ggsystem.GGSystem.getInstance()
-except:
-  system = None
-if system:
   if system.deleteGift(idgift, name):
     print "el regalo se elimino correctamente"
   else:
     print "ocurrio un error al eliminar el regalo"
-else:
-  dir = "/home/jmariscal/proyectos/genteguada/gg/GG/data/savedata/players"
-  file = os.path.join(dir,name+".serialized")
-  if not os.path.isfile(file):
-    print "ERROR!!! no se tienen datos del usuario"
-  else:
-    f = open(file, "r")
-    dict = pickle.load(f)
-    f.close()
-    list = []
-    for itemInventory in dict["inventory"]:
-      if str(itemInventory["class"]) == "GGGeneratedGift":
-        if not itemInventory["idGift"] == idgift: 
-          list.append(itemInventory)
-        else:
-          print "el regalo se elimino correctamente"
-      else:
-        list.append(itemInventory)
-  dict["inventory"] = list
-  f = open(file, "w")
-  pickle.dump(dict, f)
-  f.close()
-
-
+except:
+  print "El servidor del juego no esta levantado"
--- gghttpserver/cgi/removeplayer.py
+++ gghttpserver/cgi/removeplayer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 
 import cgi
-import os
 import GG.model.ggsystem
-import shutil
 
 print "Content-Type: text/plain\r\n\r\n"
 
@@ -11,23 +9,11 @@ form = cgi.FieldStorage()
 name = form.getvalue("user")
 try:
   system = GG.model.ggsystem.GGSystem.getInstance()
-except:
-  system = None
-if system:
   if system.deletePlayer(name):
     print "el jugador se elimino correctamente"
   else:
     print "ocurrio un error al eliminar el jugador"
-else:
-  dir = "/home/jmariscal/proyectos/genteguada/gg/GG/data/savedata/players"
-  file = os.path.join(dir,name+".serialized")
-  if not os.path.isfile(file):
-    print "ERROR!!! no se tienen datos del usuario"
-  else:
-    os.remove(file)
-    if isdir("/home/jmariscal/proyectos/genteguada/gg/GG/data/avatars/"+name):
-      shutil.rmtree("/home/jmariscal/proyectos/genteguada/gg/GG/data/avatars/"+name)
-    if isfile("/home/jmariscal/proyectos/genteguada/gg/GG/data/avatars/masks/"+name+".png")
-      os.remove("/home/jmariscal/proyectos/genteguada/gg/GG/data/avatars/masks/"+name+".png")
+except:
+  print "El servidor del juego no esta levantado"
 
 
