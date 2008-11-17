fd0790f8801692b2582c6dc3e57fca50203b5a09
mdelacalle <mdelacalle>
2008-11-17 17:05:57 +0000

    cambiando el codigo de los errores

--- gghttpserver/cgi/exportplayer.py
+++ gghttpserver/cgi/exportplayer.py
@@ -7,12 +7,14 @@ import GG.utils
 
 print "Content-Type: text/plain\r\n\r\n"
 
+#ERROR:01 => "no se han encontrado datos del usuario"
+
 form = cgi.FieldStorage()
 name = form.getvalue("user")
 SAVE_DATA_PLAYER = os.path.join(GG.utils.SAVE_DATA, "players")
 file = os.path.join(SAVE_DATA_PLAYER,name+".serialized")
 if not os.path.isfile(file):
-  print "ERROR!!! no se tienen datos del usuario"
+  print "ERROR:01"
 else:
   f = open(file, "r")
   dict = pickle.load(f)
