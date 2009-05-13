9c30cb254ec7d125aab21702b864ba8078c21420
mdelacalle <mdelacalle@b6332b80-7740-0410-afe7-f4c4f72231bb>
2009-05-13 09:45:49 +0000

    preparando nueva version

--- gg/GG/genteguada.py
+++ gg/GG/genteguada.py
@@ -13,7 +13,7 @@ import GG.utils
 import GG.isoview.guiobjects
 
 #Constants
-VERSION = "0.12.1-1"
+VERSION = "0.13.1-1"
 VERSION_TITLE = "GenteGuada "+VERSION
 CLEAR_CACHE_WEEKS = 4
 LOADING_BACKGROUND = os.path.join(GG.utils.BACKGROUNDS, "loadingGG.png")
