a99495b877a63b66681dbe27160a4c4c343e6241
stewie <stewie@b6332b80-7740-0410-afe7-f4c4f72231bb>
2010-05-27 08:35:08 +0000

    Updated version in setup.py file

--- upstream/setup.py
+++ upstream/setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setup (  
         name = "genteguada",
-        version = "0.1",
+        version = "0.17.2",
         description = "Juego colaborativo isometrico",
         author = "Eduardo Alvarado",
         author_email = "ealvarado@igosoftware.es",
