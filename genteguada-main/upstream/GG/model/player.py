4bde792e6c2d5351fc43b526147e511fa9c25573
stewie <stewie@b6332b80-7740-0410-afe7-f4c4f72231bb>
2010-06-16 11:54:37 +0000

    modified file player.py

--- genteguada-main/upstream/GG/model/player.py
+++ genteguada-main/upstream/GG/model/player.py
@@ -857,7 +857,16 @@ class GGPlayer(item_with_inventory.GGItemWithInventory):
 
   def tryOutToInventory(self, item):
     if item.inventoryOnly():
-      self.newChatMessage("Mejor no. Creo que puede ser util más adelante.", 2) 
+      if item.__class__ == GG.model.generated_inventory_item.GGGeneratedInventoryItem:
+        self.removeFromInventory(item)
+        self.setUnselectedItem()
+      elif item.__class__ == GG.model.generated_inventory_item.GGGeneratedGift:
+        newitem = GG.model.giver_npc.WebGift(item.spriteName, item.label, self.username, item.getIdGift())
+        if self.addToRoomFromInventoryNewItem(newitem):
+          self.removeFromInventory(item)
+        self.setUnselectedItem()
+      else:
+        self.newChatMessage("Mejor no. Creo que puede ser util más adelante.", 2) 
     else:   
       self.addToRoomFromInventory(item)
       self.setUnselectedItem()
@@ -880,3 +889,18 @@ class GGPlayer(item_with_inventory.GGItemWithInventory):
       return False
     else:
       return True
+
+
+  def addToRoomFromInventoryNewItem(self, item):
+    """ Removes an item from the inventory and drops it in front of the player.
+    item: item to drop.
+    """
+    dropLocation = GG.utils.getFrontPosition(self.getPosition(), self.__heading, self.getRoom().size)
+    if not self.getRoom().getTile(dropLocation).stepOn() or dropLocation == [-1, -1]:
+      self.newChatMessage("No puedo soltarlo ahí", 1)
+      return False
+    else:    
+      item_with_inventory.GGItemWithInventory.addToRoomFromInventoryNewItem(self, item, dropLocation)
+    self.save("player")
+    return True
+
